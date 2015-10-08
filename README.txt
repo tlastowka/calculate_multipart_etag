d673cb84f9320596d97fee3e589d012a46a772a1
Tony Lastowka <tlastowka@gmail.com>
2015-10-08 17:25:18 -0400

    initial

---
+++ README.txt
@@ -0,0 +1,53 @@
+DESCRPTION:
+
+	Given a file and a chunk size in megabytes, calculates what the Amazone S3 etag will be.
+
+
+
+	Amazon uses a simple md5 sum for an etag on single part uploads, but on multipart uploads
+	they use a scheme of md5ing the chunks, converting the md5s to binary,
+	and then md5ing the binary of the chunks.
+
+	This can make comparing files in s3 to local copies without downloading them a pain.
+
+	The process to generate them is just convoluted enough that there's a lot of confused people out there on the forums.
+	Lots of speculation, not much code examples other than some bash tools.
+
+	It works from the command line, or import the function into your own code and use it there.
+
+	Tested on python 3.4, but should work on 2.6+ unless I missed something obvious.
+
+	Big thanks to antespi for his bash tool that does the same thing.
+		 https://github.com/antespi/s3md5
+
+
+
+USAGE:
+	python calculate_multipart_etag.py source_path chunk_size [expected]
+
+EXAMPLES:
+
+	# just generate the etag
+
+	$ python ./calculate_multipart_etag.py 100meg.file 50
+	"67ff3c4020afd26957fe91c1891e362e-2"
+	$
+
+
+	# compare it with one you have already (success)
+
+	$ python ./calculate_multipart_etag.py 100meg.file 50 "67ff3c4020afd26957fe91c1891e362e-2"
+	"67ff3c4020afd26957fe91c1891e362e-2"
+
+	# compare it with one you have already (failure)
+
+	$ python ./calculate_multipart_etag.py 100meg.file 50 "1234567890abcdefghijklmnopqrstuv-42"
+	Traceback (most recent call last):
+	  File "./calculate_multipart_etag.py", line 81, in <module>
+	    print(calculate_multipart_etag(source_path,chunk_size,expected))
+	  File "./calculate_multipart_etag.py", line 56, in calculate_multipart_etag
+	    raise ValueError('new etag %s does not match expected %s' % (new_etag,expected))
+	ValueError: new etag "67ff3c4020afd26957fe91c1891e362e-2" does not match expected "1234567890abcdefghijklmnopqrstuv-42"
+	$
+
+
---
+++ calculate_multipart_etag.py
@@ -0,0 +1,82 @@
+# calculate_multipart_etag  Copyright (C) 2015
+#      Tony Lastowka <tlastowka at gmail dot com>
+#      https://github.com/tlastowka	
+#
+#
+# calculate_multipart_etag is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# calculate_multipart_etag is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with s3md5.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def calculate_multipart_etag(source_path, chunk_size, expected=None):
+
+    """
+    calculates a multipart upload etag for amazon s3
+
+    Arguments:
+
+    source_path -- The file to calculate the etage for
+    chunk_size -- The chunk size to calculate for.
+
+    Keyword Arguments:
+
+    expected -- If passed a string, the string will be compared to the resulting etag and raise an exception if they don't match
+
+
+    """
+
+    import hashlib
+    md5s = []
+
+    keep_going = True
+    with open(source_path,'rb') as fp:
+        while keep_going:
+
+            data = fp.read(chunk_size)
+
+            if not data:
+                break
+            md5s.append(hashlib.md5(data))
+
+    digests = b"".join(m.digest() for m in md5s)
+
+    new_md5 = hashlib.md5(digests)
+    new_etag = '"%s-%s"' % (new_md5.hexdigest(),len(md5s))
+    if expected:
+        if not expected==new_etag:
+            raise ValueError('new etag %s does not match expected %s' % (new_etag,expected))
+
+    return new_etag
+
+if __name__ == '__main__':
+
+	import sys
+
+
+	if  len(sys.argv) < 3 or len(sys.argv) > 4:
+
+		print("python %s source_path chunk_size [expected]" % (sys.argv[0]))
+		exit()
+
+	source_path = sys.argv[1]
+	chunk_size = sys.argv[2]
+	chunk_size = int(chunk_size) * 1024 * 1024
+
+
+	try:
+		expected = '"%s"' % (sys.argv[3])
+	except Exception as e:
+		expected = None
+
+
+	print(calculate_multipart_etag(source_path,chunk_size,expected))
+
